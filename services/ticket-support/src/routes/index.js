94a32be227cdf5f5b3a6a7034b7e0d1db9f0a196
radhaCumulative <radhacumulative@gmail.com>
2025-08-08 11:36:18 +0530

    Update index.js

--- services/ticket-support/src/routes/index.js
+++ services/ticket-support/src/routes/index.js
@@ -1,17 +1,18 @@
-import express from 'express';
+import express from "express";
 
 // Import your individual route modules
-import userRoutes from './user/index.js';
-import roleRoutes from './role/index.js';
-
+import userRoutes from "./user/index.js";
+import roleRoutes from "./role/index.js";
+import ticketRoutes from "./ticket/index.js";
 const router = express.Router();
 
 // Prefix your routes here
-router.use('/users', userRoutes);      // /api/users
-router.use('/role', roleRoutes)
-// Health check route
-router.get('/health', (req, res) => {
-  res.json({ success: true, message: 'API is healthy 🚀' });
-});
+router.use("/users", userRoutes); // /api/users
+router.use("/role", roleRoutes);
+router.use("/tickets", ticketRoutes),
+  // Health check route
+  router.get("/health", (req, res) => {
+    res.json({ success: true, message: "API is healthy 🚀" });
+  });
 
 export default router;
