a8d8b7fc12035d79b0fc8a44efda2440f85d7457
radhaCumulative <radhacumulative@gmail.com>
2025-10-31 16:32:04 +0530

    fixed

--- services/analytics-dashboard/src/middlewares/security.js
+++
@@ -1,9 +0,0 @@
-import cors from 'cors';
-import helmet from 'helmet';
-import morgan from 'morgan';
-
-export const securityMiddleware = (app) => {
-  app.use(helmet());
-  app.use(cors());
-  app.use(morgan('dev'));
-};
--- services/analytics-dashboard/src/utils/safeAwait.js
+++
@@ -1,9 +0,0 @@
-export const safeAwait = async (promise) => {
-    try {
-      const data = await promise;
-      return [null, data];
-    } catch (error) {
-      return [error, null];
-    }
-  };
-  
